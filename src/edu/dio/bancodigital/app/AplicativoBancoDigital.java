92748f6130bf87f3e745c9b692b48f0396312cbb
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-05-19 12:11:09 -0300

    adicao conta corrente e conta poupança

---
+++ src/edu/dio/bancodigital/app/AplicativoBancoDigital.java
@@ -0,0 +1,20 @@
+package edu.dio.bancodigital.app;
+
+import edu.dio.bancodigital.dominio.Banco;
+import edu.dio.bancodigital.dominio.Conta;
+
+import java.util.SortedSet;
+
+public class AplicativoBancoDigital {
+
+
+    public static void main(String[] args) {
+
+        Banco banco = new Banco();
+        SortedSet<Conta> contas;
+
+
+
+
+    }
+}
